840db3ad0c49f2b881c770683b6d252a05a96b14
Rinsyl.Furia <rmfuria2031@crc.pshs.edu.ph>
2026-08-06 09:26:10 +0800

    Update main (2).py

--- main (2).py
+++ main (2).py
@@ -1,17 +1,18 @@
 import math
+
+# Input the coordinates of the first point.
 x1 = float(input("Enter the value of x1: "))
-x2 = float(input("Enter the value of x2: "))
 y1 = float(input("Enter the value of y1: "))
+
+# Input the coordinates of the second point.
+x2 = float(input("Enter the value of x2: "))
 y2 = float(input("Enter the value of y2: "))
 
-x= math.pow(((x1-x2)), 2)
-y= math.pow(((y1-y2)), 2)
+# Find out the distance using the distance formula.
+distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
 
-print(x)
-print(y)
+print(f"The distance between the two points is: {distance:.2f}")
 
-distance= round(math.sqrt(x + y), 2)
-print ("The distance between the two given points is:", distance)
 # It helped simplify the program because there were many functions.
 # The functions sqrt and pow were much easier to use.
 # Without sqrt and pow it would take much longer to finish the code.
